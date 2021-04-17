fe06bab8a22965d71ff930ce7d38e0a95a854db0
Anmol-techie <69382513+Anmol-techie@users.noreply.github.com>
2021-04-17 22:37:27 +0530

    created app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+import os
+from werkzeug.utils import secure_filename
+from tensorflow.keras.models import load_model
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+
+UPLOAD_FOLDER = './flask app/assets/images'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+# Create Database if it doesnt exist
+
+app = Flask(__name__,static_url_path='/assets',
+            static_folder='./flask app/assets', 
+            template_folder='./flask app')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
